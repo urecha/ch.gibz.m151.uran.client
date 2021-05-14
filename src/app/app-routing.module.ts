b3ad64ada098901ee53bbc256ce0a1786ed95db3
URAN <urecha@emea.roche.com>
2021-05-14 09:17:07 +0200

    refactor app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryComponent } from './dashboard/gallery.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+     path: 'gallery', component: GalleryComponent
+  },
+  {
+     path: '', redirectTo: 'gallery', pathMatch: 'full' 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
