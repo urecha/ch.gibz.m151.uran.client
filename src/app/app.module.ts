46993cc19120c5e7296338f390258ae565e08d17
URAN <urecha@emea.roche.com>
2021-05-14 09:15:01 +0200

    refactorings app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { GalleryComponent } from './dashboard/gallery.component';
 
-import { ExhibitService } from './services/exhibit.service';
-import { CommentService } from './services/comment.service';
+import { ExhibitService } from './services/exhibit/exhibit.service';
+import { CommentService } from './services/comment/comment.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
